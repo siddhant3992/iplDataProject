04bd98f1beb14e4301f84ec9485674013bff8367
siddhant3992 <40051645+siddhant3992@users.noreply.github.com>
2018-07-09 18:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function getFiles(path) {
 
 function plotGraph() {
     let gt = getFiles("/jsonFile/matchesPerYear.json");
-    let chart = Highcharts.chart("container", {
+    let chart = new Highcharts.chart("container", {
         chart: {
             type: "column"
         },
